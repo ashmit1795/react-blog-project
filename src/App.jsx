d6f64d75e55069bbe34ca9db7b6e352d0f227efe
ashmit1795 <ashmitpatra77@gmail.com>
2025-06-07 22:41:54 +0530

    Integrate Redux for authentication management and enhance App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import debug from 'debug';
+import { Header, Footer } from './components';
+import { Outlet } from 'react-router-dom';
+
+
+const appDebug = debug("App")
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        useDispatch(login({userData}));
+      } else {
+        useDispatch(logout());
+      }
+    }).catch((error) => {
+      appDebug("App :: error", error);
+    }).finally(() => {
+      setLoading(false);
+    })
+  }, []);
+
+  if(loading){
+    return <div className="loading">Loading...</div>
+  }
   return (
-    <>
-      <h1>React with Appwrite</h1>
-    </>
-  )
+    <div className="min-h-screen flex flex-wrap justify-center content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO
+          {/* 
+            TODO: <Outlet /> 
+          */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App
