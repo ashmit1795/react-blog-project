73f80aaea3a1191abc1ca7ba5429ddc23f188c2f
ashmit1795 <ashmitpatra77@gmail.com>
2025-06-07 17:28:25 +0530

    Add StorageService class for handling file operations in AppWrite

---
+++ src/appwrite/storage.js
@@ -0,0 +1,67 @@
+import { ID, Client, Storage } from "appwrite";
+import config from "../config/config";
+import debug from "debug";
+
+const storageDebug = debug("appwrite:storage");
+
+export class StorageService{
+    client = new Client();
+    storage;
+
+    constructor(){
+        this.client.setEndpoint(config.appwriteProjectId).setProject(config.appwriteProjectId);
+        this.storage = new Storage(this.client);
+    }
+
+    async uploadFile(file){
+        try {
+            const result = await this.storage.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+            
+            if (result) return result;
+            else return null;
+        } catch (error) {
+            storageDebug("uploadFile :: error", error);
+            throw error;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            const result = await this.storage.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            )
+            
+            if (result) return result;
+            else return null;
+            
+        } catch (error) {
+            storageDebug("deleteFile :: error", error);
+            throw error;
+        }
+    }
+
+    getFilePreview(fileId){
+        try {
+            const result = this.storage.getFilePreview(
+                config.appwriteBucketId,
+                fileId
+            )
+            
+            if (result) return result;
+            else return null;
+            
+        } catch (error) {
+            storageDebug("getFilePreview :: error", error);
+            throw error;
+        }
+    }
+}
+
+const storageService = new StorageService();
+
+export default storageService;
