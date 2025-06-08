ff82062a6577f0822a303a91f8de3b18d4232bb5
ashmit1795 <ashmitpatra77@gmail.com>
2025-06-08 17:19:33 +0530

    Enhance Footer and Header components with responsive layout and navigation links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import React from 'react';
+import {Container, Logo, LogoutBtn} from "../index";
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 function Header() {
+  const authStatus = useSelector((state) => {
+    state.auth.status
+  });
+
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      endpoint: "/",
+      active: true
+    },
+    {
+      name: "Login",
+      endpoint: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      endpoint: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      endpoint: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      endpoint: "/add-post",
+      active: authStatus,
+    },
+  ];
+
   return (
-    <div>Header</div>
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link>
+              <Logo width="70px"/>
+            </Link>
+          </div>
+          <ul className="flex ml-auto ">
+            {navItems.map((item) => 
+              item.active ? <li key={item.name}>
+                <button 
+                  className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  onClick={() => navigate(item.endpoint)}
+                >
+                  {item.name}
+                </button>
+              </li> : null
+            )}
+            
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
 
