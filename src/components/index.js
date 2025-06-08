7ab45ab3340287cfba8d64265724ca72a4194837
ashmit1795 <ashmitpatra77@gmail.com>
2025-06-08 17:19:46 +0530

    Update component index to include Container, LogoutBtn, and Input

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,10 @@
+import Button from "./Button/Button";
+import Container from "./Container/Container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input/Input";
+import Logo from "./Logo/Logo";
 
-export {Footer, Header}
+
+export {Footer, Header, Container, Logo, LogoutBtn, Button, Input}
