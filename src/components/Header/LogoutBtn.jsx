06aa6362de3acc2d9866377b4dbc4fb5b609d4ab
ashmit1795 <ashmitpatra77@gmail.com>
2025-06-08 17:19:16 +0530

    Add Button, Container, LogoutBtn, Input, and Logo components

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import authService from '../../appwrite/auth';
+import {logout} from '../../store/authSlice'
+import { useDispatch } from 'react-redux';
+
+function LogoutBtn() {
+
+    const logoutHandler = () => {
+        authService.logout().then(
+            () => {
+                useDispatch(logout());
+            }
+        )
+    }
+
+
+    return (
+        <div 
+            className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+            onClick={logoutHandler}
+        >
+            Logout
+        </div>
+    )
+}
+
+export default LogoutBtn
