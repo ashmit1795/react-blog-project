3b361ba3974702d056cf448d8a9b071465f037bd
ashmit1795 <ashmitpatra77@gmail.com>
2025-06-10 23:04:19 +0530

    Refactor Header component to rectify authStatus selector

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
 import {Container, Logo, LogoutBtn} from "../index";
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 
 function Header() {
-  const authStatus = useSelector((state) => {
-    state.auth.status
-  });
+  const authStatus = useSelector((state) => state.auth.status);
 
   const navigate = useNavigate();
 
